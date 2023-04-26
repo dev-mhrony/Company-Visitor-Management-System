747d6dc857385a36c16d75ac4fc9f4db91605d11
dev.mhrony <mh.rony.dhanvi@gmail.com>
2023-04-26 22:24:46 +0600

    Last Update - 27/04/2023

---
+++ Database/ccbd_visitor.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2023 at 05:56 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cvdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `permission` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `createuser` varchar(255) DEFAULT NULL,
+  `deleteuser` varchar(255) DEFAULT NULL,
+  `createbid` varchar(255) DEFAULT NULL,
+  `updatebid` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `permission`, `createuser`, `deleteuser`, `createbid`, `updatebid`) VALUES
+(1, 'Superuser', '1', '1', '1', '1'),
+(2, 'Admin', '1', '1', '1', '1'),
+(3, 'User', NULL, NULL, '1', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `Staffid` varchar(255) DEFAULT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Status` int(11) NOT NULL DEFAULT 1,
+  `Photo` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT 'avatar15.jpg',
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `Staffid`, `AdminName`, `UserName`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Status`, `Photo`, `Password`, `AdminRegdate`) VALUES
+(2, 'C001', 'Admin', 'ccbd', 'Code Camp', 'BD ', 8801608445456, 'codecampbdofficial@gmail.com', 1, '03.webp', '74163f089953fb398d3d5ece038a2613', '2020-07-21 10:18:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblvisitor`
+--
+
+CREATE TABLE `tblvisitor` (
+  `ID` int(5) NOT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `Address` varchar(250) DEFAULT NULL,
+  `WhomtoMeet` varchar(120) DEFAULT NULL,
+  `Deptartment` varchar(120) DEFAULT NULL,
+  `ReasontoMeet` varchar(120) DEFAULT NULL,
+  `EnterDate` timestamp NULL DEFAULT current_timestamp(),
+  `remark` varchar(255) DEFAULT NULL,
+  `outtime` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblvisitor`
+--
+ALTER TABLE `tblvisitor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `tblvisitor`
+--
+ALTER TABLE `tblvisitor`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','cvdb');
+define('DB_NAME','ccbd_visitor');
 // Establish database connection.
 try
 {
